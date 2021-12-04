1b2c4d0925a97da09b2a7b3474904b4681e9661b
IndraProgrammer <74811046+IndraProgrammer@users.noreply.github.com>
2021-12-04 12:28:33 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,15 +52,9 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
-noseX="";
-noseY="";
-gamestatus="";
-function startgame(){
-  document.getElementById("status").innerHTML="game is loading";
-  onclick(startgame);
-}
+
 function game(){
-console.log("noseX="+noseX+"noseY"+noseY);
+
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
@@ -73,7 +67,7 @@ console.log("noseX="+noseX+"noseY"+noseY);
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press the Play button to start and play the game ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -121,8 +115,7 @@ console.log("noseX="+noseX+"noseY"+noseY);
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  console.log("start");
-  if(gamestatus=="start" && noseX!="" && gameConfig.status==="start") {
+  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -286,13 +279,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(noseX<300){
+    if(keyDown(control.left)){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(noseX>300){
+    if(keyDown(control.right)){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -307,7 +300,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if((noseY<150)&&character.live || (touchIsDown&&character.live) ){
+	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
